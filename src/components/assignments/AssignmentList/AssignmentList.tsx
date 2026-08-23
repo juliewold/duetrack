d04290027624a58bbab1ed24cb9dee51f49177fe
Julie Wold <juliewold2006@gmail.com>
2026-08-23 18:19:56 +0200

    style: add assignment list layout

--- src/components/assignments/AssignmentList/AssignmentList.tsx
+++ src/components/assignments/AssignmentList/AssignmentList.tsx
@@ -1,10 +1,11 @@
+import "./AssignmentList.css";
 import AssignmentCard from "../AssignmentCard/AssignmentCard";
 import { assignments } from "../../../data/assignments";
 import { courses } from "../../../data/courses";
 
 function AssignmentList() {
   return (
-    <>
+    <div className="assignment-list">
       {assignments.map((assignment) => {
         const course = courses.find(
           (course) => course.id === assignment.courseId,
@@ -22,7 +23,7 @@ function AssignmentList() {
           />
         );
       })}
-    </>
+    </div>
   );
 }
 
